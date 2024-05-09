2bf5e668dfba7d58f24b4b9c584c38bbdc285b14
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-09 10:37:13 +0200

    feat: add prelim plot

--- src/inspect_subset.py
+++ src/inspect_subset.py
@@ -1,6 +1,42 @@
+import pathlib
 import pandas as pd
 
-# read the data
-norreport_1A = pd.read_csv('data/1A_norreport.csv', parse_dates=['date', 'scheduledarrivetime'])
+def main():
+    path = pathlib.Path(__file__)
+    plot_dir = path.parents[1] / "plots"
+    plot_dir.mkdir(exist_ok=True, parents=True)
 
-print(len(norreport_1A))
+    # read the data
+    norreport_1A = pd.read_csv('data/1A_norreport.csv')
+
+    # bind date and scheduledarrivetime to a single datetime column
+    norreport_1A['datetime'] = norreport_1A['date'] + " " + norreport_1A['scheduledarrivetime']
+
+    # sort by datetime
+    norreport_1A = norreport_1A.sort_values('datetime').reset_index(drop=True)
+
+    # remove rows missing in actualarrivaltime
+    norreport_1A = norreport_1A.dropna(subset=['actualarrivetime'])
+    
+    # remove duplicates 
+    print(len(norreport_1A))
+    norreport_1A = norreport_1A.drop_duplicates()
+    print(len(norreport_1A))
+
+    
+
+    # save 
+    norreport_1A.to_csv('data/processed_1A_norreport.csv', index=False)
+
+    # subset to only first 1000 rows
+    norreport_1A = norreport_1A.head(1000)
+
+    # plot the timeseries for Cumulative
+    ts_plot = norreport_1A.plot(x="datetime", y="Cumulative", figsize=(15,5))
+
+    # save to plot dir
+    ts_plot.get_figure().savefig(plot_dir / "norreport_1A_ts.png")
+
+
+if __name__ == "__main__":
+    main()
