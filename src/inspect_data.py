23c16cb46b2f9829cb53d56d8f9c8c69a6b47b60
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-09 13:02:50 +0200

    refactor: subset data in one file, add more ts points to

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -8,6 +8,7 @@ def main():
 
     # read the data
     norreport_1A = pd.read_csv('data/1A_norreport.csv')
+    print(norreport_1A.head())
 
     # bind date and scheduledarrivetime to a single datetime column
     norreport_1A['datetime'] = norreport_1A['date'] + " " + norreport_1A['scheduledarrivetime']
@@ -17,22 +18,22 @@ def main():
 
     # remove rows missing in actualarrivaltime
     norreport_1A = norreport_1A.dropna(subset=['actualarrivetime'])
+
+    norreport_1A = norreport_1A.dropna(subset=['actualdeparturetime'])
     
     # remove duplicates 
     print(len(norreport_1A))
     norreport_1A = norreport_1A.drop_duplicates()
     print(len(norreport_1A))
 
-    
-
     # save 
     norreport_1A.to_csv('data/processed_1A_norreport.csv', index=False)
 
     # subset to only first 1000 rows
-    norreport_1A = norreport_1A.head(1000)
+    norreport_1A = norreport_1A.head(10000)
 
     # plot the timeseries for Cumulative
-    ts_plot = norreport_1A.plot(x="datetime", y="Cumulative", figsize=(15,5))
+    ts_plot = norreport_1A.plot(x="datetime", y="Cumulative", figsize=(25,10))
 
     # save to plot dir
     ts_plot.get_figure().savefig(plot_dir / "norreport_1A_ts.png")
