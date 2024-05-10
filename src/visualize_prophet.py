96cd66455b59f7b1c6cd16ce709defac00334294
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-10 14:25:41 +0200

    feat: plot seasonality parameters

--- src/visualize_prophet.py
+++ src/visualize_prophet.py
@@ -1,29 +1,37 @@
 import pathlib
 import pandas as pd 
 from neuralprophet import NeuralProphet, set_log_level
+import matplotlib.pyplot as plt
 
 def main():
     path = pathlib.Path(__file__)
 
     data_path = path.parents[1] / "data" 
+    plot_dir = path.parents[1] / "plots"
+    plot_dir.mkdir(exist_ok=True, parents=True)
 
     # read the data
     norreport_1A = pd.read_csv(data_path / 'processed_1A_norreport.csv')
 
     # init neural prophet model
-    model = NeuralProphet()
+    model = NeuralProphet(
+                        yearly_seasonality = True,  
+                        weekly_seasonality = True, 
+                        daily_seasonality = True
+                        )
 
-    # set plotting backend to resampler
-    model.set_plotting_backend('plotly-resampler')
+    # fit the model with 30-minute frequency
+    metrics = model.fit(norreport_1A, freq='30min')
 
-    # subset data and rename for neural prophet
-    model_data = norreport_1A[['datetime', 'Cumulative']]
-    model_data.columns = ['ds', 'y']
+    print(metrics)
 
-    # fit the model
-    model.fit(model_data)
+    # forecast
+    forecast = model.predict(norreport_1A)
 
-
+    # plot the seasonality components
+    plot = model.plot_parameters(components=["seasonality"])
+    
+    plot.write_image(file = str(plot_dir / "seasonality_components.png"), format = "png")
 
 
 if __name__ == "__main__":
