400149bcf76bc55e1b036558710a70a212f08739
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-10 14:48:10 +0200

    feat: add prelim autocorrelation

--- src/autocorrelation.py
+++ src/autocorrelation.py
@@ -1,16 +1,23 @@
 import pathlib
 import pandas as pd
-from datetime import datetime, timedelta
+from statsmodels.graphics.tsaplots import plot_acf
 
 def main():
     # set paths
     path = pathlib.Path(__file__)
     data_dir = path.parents[1] / "data"
+    plot_dir = path.parents[1] / "plots"
 
     # load data
     data = pd.read_csv(data_dir / "processed_1A_norreport.csv")
 
-    #
+    # create autocorrelation plot
+    print("[INFO:] Creating autocorrelation plot")
+    autocor_plot = plot_acf(data["y"], lags=50)
+
+    # save plot
+    autocor_plot.savefig(plot_dir / "autocorrelation.png")
+
 
 if __name__ == "__main__":
     main()
