944ab17c36b566cad876a75c11ccfacad63e513f
Anton Drasbæk <202008161@post.au.dk>
2024-05-14 14:42:13 +0200

    standardize format, chang names, doc strings

--- src/data_utils.py
+++ src/data_utils.py
@@ -18,9 +18,20 @@ def read_data(data_path, chunksize:int=None, sep=";"):
 
     return df    
 
-def split_timeseries_data(X, gap:int=48, test_size:int=48, max_train_size:int=10):
+def split_sliding_window(X:pd.Series, gap:int=48, test_size:int=48, max_train_size:int=10):
     '''
-    function to split the data into n_splits using TimeSeriesSplit
+    Split the data into small windows of train and test (sliding window approach). 
+    Uses TimeSeriesSplit from sklearn.
+
+    Args
+        X: data 
+        gap: gap between train and test
+        test_size: size of the test set
+        max_train_size: maximum size of the training set
+
+    Returns
+        train_inds: dictionary with train indices
+        test_inds: dictionary with test indices
     '''
     # identify the length of a full fold
     fold_length = gap + test_size + max_train_size
@@ -37,17 +48,24 @@ def split_timeseries_data(X, gap:int=48, test_size:int=48, max_train_size:int=10
     )
     data_generator = tscv.split(X)
 
-    return data_generator
+    # create lists of lists for train and test indices
+    train_inds = {}
+    test_inds = {}
 
-def split_timeseries_data_rolling_origin(X, gap:int=24, test_size:int=36, steps:int=1):
+    # iterate over the generator object
+    for i, (train_ind, test_ind) in enumerate(data_generator):
+        train_inds[i] = train_ind
+        test_inds[i] = test_ind
+
+    return train_inds, test_inds
+
+def split_rolling_origin(X, gap:int=24, test_size:int=36, steps:int=1, min_train_size:int=24*7):
     '''
     function to split the data into n_splits using TimeSeriesSplit
     '''
-    # identify the length of a full fold
-    fold_length = gap + test_size
 
-    # subtract from length of data to get the number of folds
-    n_splits = (int(len(X) - fold_length) // test_size)
+    # subtract from length of data to get the number of folds1
+    n_splits = (int(len(X) - min_train_size) // test_size)
 
     tscv = TimeSeriesSplit(
         n_splits=n_splits,
@@ -90,13 +108,12 @@ def main():
     gap = 24
     max_train_size = 24 * 7 * 2 # 24 hours x 7 (days) x 2 (weeks)
     #generator = split_timeseries_data(df['ds'], gap=gap, test_size=48, max_train_size=max_train_size)
-    train_inds, test_inds = split_timeseries_data_rolling_origin(df['ds'], gap=gap, test_size=36, steps=2)
+    train_inds, test_inds = split_rolling_origin(df['ds'], gap=gap, test_size=36, steps=4, min_train_size=24*7)
 
-    print(train_inds.keys())
+    # print the test_inds for key 0 and 552
+    print(train_inds[0])
+    print(test_inds[0])
 
-    
-    
-    
 
 if __name__ == "__main__":
     main()
