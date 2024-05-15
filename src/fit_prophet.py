a747b57db3dacafd8af7db135671b9e6ff694354
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-15 10:08:09 +0200

    fix: combine param grids instead of having 2

--- src/fit_prophet.py
+++ src/fit_prophet.py
@@ -5,31 +5,31 @@ from neuralprophet import NeuralProphet
 from sklearn.model_selection import ParameterGrid
 import random
 from data_utils import split_rolling_origin
+import math
 
-def cv_single_fold(train_ind, test_ind, df, init_params, fit_params, freq):
+def cv_single_fold(train_ind, test_ind, df, params, freq):
     '''
     Process a single fold of cross-validation using provided parameters.
     '''
     df_train = df.iloc[train_ind]
     df_test = df.iloc[test_ind]
     
+    # combine init and default parameters
     default_params = {
         "yearly_seasonality": False, # set yearly seasonality also to avoid model having to check for it 
         'weekly_seasonality': True,
         'daily_seasonality': True,
+        'newer_samples_start': 0.5
     }
+    all_params = {**default_params, **params}
 
-    # combine default parameters with the initialization grid
-    params = {**default_params, **init_params}
+    # init model with parameters from the initialization grid
+    model = NeuralProphet(**all_params)
 
-    # Initialize model with parameters from the initialization grid
-    model = NeuralProphet(**params)
-    
-    # Fit and test the model with parameters for the fit method
-    train_metrics = model.fit(df_train, freq=freq, **fit_params)
+    train_metrics = model.fit(df_train, freq=freq)
     val_metrics = model.test(df_test, verbose=False)
 
-    # get the mae from both the training and validation set
+    # get last MAE from training and (only) MAE from validation
     mae_train = train_metrics['MAE'].values[-1]
     mae_val = val_metrics['MAE_val'].values[0]
     
@@ -44,9 +44,34 @@ def get_mean_sd(vals:list):
 
     return mean, sd
 
-def cross_validate(df, train_inds:dict, test_inds:dict, init_params:dict, fit_params:dict, freq="1h", n_cores:int=1):
+def sample_parameter_combinations(n_combinations, param_grid):
+    # Create a list of all possible combinations from the parameter grid
+    all_combinations = list(ParameterGrid(param_grid))
+
+    # check if n_combinations is larger than the number of possible combinations
+    if n_combinations > len(all_combinations):
+        print("Warning: Requested more combinations than possible. Returning all combinations.")
+        sampled_combinations = all_combinations
+    
+    # Sample n_combinations from the list, ensuring not to exceed the number of available combinations
+    if len(all_combinations) < n_combinations:
+        print("Warning: Requested more combinations than possible. Returning all combinations.")
+        sampled_combinations = all_combinations
+    else:
+        sampled_combinations = random.sample(all_combinations, n_combinations)
+    
+    # announce sampled combinations
+    print(f"Sampled {len(sampled_combinations)} parameter combinations.")
+
+    print("Sampled parameter combinations:")
+    for i, params in enumerate(sampled_combinations):
+        print(f"Combination {i+1}: {params}")
+
+    return sampled_combinations
+
+def cross_validate(df, train_inds:dict, test_inds:dict, params:dict, freq="1h", n_cores:int=1):
     '''
-    Cross validate the model with given hyperparameters.
+    Cross validate the model with given hyperparameters using multiprocessing.
 
     Args:
         df: dataframe with the data
@@ -62,11 +87,11 @@ def cross_validate(df, train_inds:dict, test_inds:dict, init_params:dict, fit_pa
     '''
     processes = []
     for (train_ind, test_ind) in zip(train_inds.values(), test_inds.values()):
-        args = (train_ind, test_ind, df, init_params, fit_params, freq)
+        args = (train_ind, test_ind, df, params, freq)
         processes.append(args)
     
     with mp.Pool(n_cores) as pool:
-        mae_trains, mae_vals = zip(*pool.starmap(cv_single_fold, processes))
+        mae_trains, mae_vals = zip(*pool.starmap(cv_single_fold, processes)) # unpack results
 
     mean_mae_train, sd_mae_train = get_mean_sd(mae_trains)
     mean_mae_val, sd_mae_val = get_mean_sd(mae_vals)
@@ -79,45 +104,42 @@ def main():
     df = pd.read_csv(data_path / 'processed_1A_norreport.csv')
 
     # hyperparameters to explore
-    init_param_grid = {
-        'n_lags': [1, 2],
+    param_grid = {
+        'n_lags': [1, 6],
         'newer_samples_weight': [2, 4],
-        'newer_samples_start': [0.5],
         'ar_layers': [[1], [32, 16]],
-        'seasonality_reg': [0.1, 0.5]
-    }
-
-    fit_param_grid = {
+        'seasonality_reg': [0.1, 0.5],
         'learning_rate': [0.05, 0.15, 0.3],
         'batch_size': [24, 48],
         'epochs': [1],
     }
-    
-    # sample all combinations and sample randomly from them if necessary
-    init_combinations = list(ParameterGrid(init_param_grid))
-    fit_combinations = list(ParameterGrid(fit_param_grid))
-    
-    sampled_init_combinations = random.sample(init_combinations, min(len(init_combinations), 2))  
-    sampled_fit_combinations = random.sample(fit_combinations, min(len(fit_combinations), 2))  
 
-    # wplit data
-    train_inds, test_inds = split_rolling_origin(df['ds'], gap=24, test_size=36, steps=4, min_train_size=24*7)
+
+    # sample parameter combinations
+    n_param_combinations = 5
+    sampled_combinations = sample_parameter_combinations(n_param_combinations, param_grid)
+
+    # data setup
+    gap = 24 # gap between train and val and train and test
+    steps = 4 # how fast rolling origin moves
+    test_size = 36 # hours 
+    min_train_size = 24*7 # i.e. 1 week 
+
+    train_inds, val_inds, _ = split_rolling_origin(df['ds'], gap=gap, test_size=test_size, steps=steps, min_train_size=min_train_size) # no need for test_inds here!
 
     results = []
     # cross-validation for each sampled parameter set
-    for init_params in sampled_init_combinations:
-        for fit_params in sampled_fit_combinations:
-            mean_mae_train, sd_mae_train, mean_mae_val, sd_mae_val = cross_validate(df, train_inds, test_inds, init_params, fit_params, freq="1h", n_cores=mp.cpu_count() - 1)
+    for params in sampled_combinations:
+        mean_mae_train, sd_mae_train, mean_mae_val, sd_mae_val = cross_validate(df, train_inds, val_inds, params, freq="1h", n_cores=mp.cpu_count() - 1)
             
-            # add the results to the DataFrame
-            results.append({
-                'init_params': init_params,
-                'fit_params': fit_params,
-                'mean_mae_train': mean_mae_train,
-                'sd_mae_train': sd_mae_train,
-                'mean_mae_val': mean_mae_val,
-                'sd_mae_val': sd_mae_val
-            })
+        # add the results to the DataFrame
+        results.append({
+            'parameters': params,
+            'mean_mae_train': mean_mae_train,
+            'sd_mae_train': sd_mae_train,
+            'mean_mae_val': mean_mae_val,
+            'sd_mae_val': sd_mae_val
+        })
 
     results_df = pd.DataFrame(results)
     results_dir = path.parents[1] / 'results'
