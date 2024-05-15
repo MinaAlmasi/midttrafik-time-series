9df96103e9b1ba2169cb532d7a3d5b70aabd9423
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-15 11:26:56 +0200

    feat: mv autocorrelation out in diff function & add skeleton of PCAF

--- src/autocorrelation.py
+++ src/autocorrelation.py
@@ -1,18 +1,12 @@
 import pathlib
 import pandas as pd
-from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import pacf
 import matplotlib.pyplot as plt
 
-def main():
-    # set paths
-    path = pathlib.Path(__file__)
-    data_dir = path.parents[1] / "data"
-    plot_dir = path.parents[1] / "plots"
 
-    # load data
-    data = pd.read_csv(data_dir / "processed_1A_norreport.csv")
-
-    # create autocorrelation plot
+def plot_autocorrelation(data, plot_dir):
+     # create autocorrelation plot
     n_lags = 48 * 7 * 2 # 24 hours x 7 (days) x 2 (weeks)
 
     print("[INFO:] Creating autocorrelation plot")
@@ -33,6 +27,24 @@ def main():
     # rm whitespace
     fig.tight_layout()
     fig.savefig(plot_dir / "norreport_1A_autocorrelation.png", dpi=300)  
+
+
+def plot_partial_autocorrelation(data, plot_dir):
+    pass
+
+def main():
+    # set paths
+    path = pathlib.Path(__file__)
+    data_dir = path.parents[1] / "data"
+    plot_dir = path.parents[1] / "plots"
+
+    # load data
+    data = pd.read_csv(data_dir / "processed_1A_norreport.csv")
+
+    # plot autocorrelation
+    plot_autocorrelation(data, plot_dir)
+
+   
 
 if __name__ == "__main__":
     main()
