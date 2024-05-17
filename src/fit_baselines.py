8d1fd085d94c45410f47b818e5c14f53e589408e
Anton Drasbæk <202008161@post.au.dk>
2024-05-17 10:24:38 +0200

    changed impute missing to use common function from utils

--- src/fit_baselines.py
+++ src/fit_baselines.py
@@ -1,21 +1,7 @@
 import pathlib
 import pandas as pd
-from data_utils import split_rolling_origin
+from data_utils import split_rolling_origin, impute_missing
 import matplotlib.pyplot as plt
-
-def impute_missing(df, method='rolling', window=20):
-    '''
-    Impute missing values in a dataframe using a specified method.
-    '''
-    if method == 'linear':
-        df['y'] = df['y'].interpolate(method='linear', limit_direction='both', limit=window)
-    elif method == 'rolling':
-        df['y'] = df['y'].interpolate(method='linear', limit_direction='both', limit=window)
-    else:
-        raise ValueError(f"Method {method} not recognized. Please use 'linear' or 'rolling'.")
-
-    return df
-
 
 def get_splits(df, train_inds, val_inds, test_inds):
     '''
@@ -247,7 +233,7 @@ def main():
     df = pd.read_csv(data_path / 'processed_1A_norreport.csv')
 
     # impute missing values
-    df = impute_missing(df, method='rolling', window=20)
+    df = impute_missing(df, method='rolling', window=12)
 
     # split the data
     gap = 24
--- src/refit_prophet.py
+++ src/refit_prophet.py
@@ -1,7 +1,7 @@
 import pathlib
 from neuralprophet import NeuralProphet
 import pandas as pd
-from data_utils import split_rolling_origin
+from data_utils import split_rolling_origin, impute_missing
 
 def refit_model(df_full_train:pd.DataFrame, params:dict, freq="1h"):
     '''
@@ -12,7 +12,7 @@ def refit_model(df_full_train:pd.DataFrame, params:dict, freq="1h"):
         "yearly_seasonality": False, # set yearly seasonality also to avoid model having to check for it 
         'weekly_seasonality': True,
         'daily_seasonality': True,
-        'newer_samples_start': 0.5
+        'newer_samples_start': 0.5,
     }
     all_params = {**default_params, **params}
 
@@ -28,44 +28,72 @@ def evaluate_model(df_test, model):
     '''
     Evaluate the model on the test set.
     '''
-    results = model.test(df_test)
+    metrics = model.test(df_test)
 
-    return results
+    return metrics
 
 def main(): 
+    # set paths
     path = pathlib.Path(__file__)
     data_path = path.parents[1] / "data"
+    results_path = path.parents[1] / "results"
+    neuralprophet_path = results_path / "neuralprophet"
+
+    # load data and impute missing
     df = pd.read_csv(data_path / 'processed_1A_norreport.csv')
+    df = impute_missing(df, method='rolling', window=12)
 
-    # temp 
-    params = {"n_lags": 2, "newer_samples_weight": 4, "newer_samples_start": 0.5, "ar_layers": [32, 16], "seasonality_reg": 0.5, "learning_rate": 0.15, "batch_size": 24, "epochs": 1}
+    # load the results file from the grid search
+    np_results = pd.read_csv(neuralprophet_path / 'np_gridsearch_20240516_160815.csv')
 
-    # data setup
+    # select top n models based on validation rmse
+    top_n_models = 5
+    best_models = np_results.sort_values(by="mean_rmse_val").iloc[:top_n_models]
+
+    # get the n best parameters from "model" column as a dict
+    top_n_params = best_models['model'].apply(eval).tolist()
+
+    # specify the data setup
     gap = 24 # gap between train and val and train and test
     steps = 4 # how fast rolling origin moves
     test_size = 36 # hours 
     min_train_size = 24*7 # i.e. 1 week 
 
+    # load the test data
     _, _, test_inds = split_rolling_origin(df['ds'], gap=gap, test_size=test_size, steps=steps, min_train_size=min_train_size)
 
     # add gap to the test indices
     dropped_inds = df.index[-(len(test_inds) + gap-1):].tolist()
 
+    # get full train data (all data except test data and gap)
     df_full_train = df.drop(index=dropped_inds, axis=0)
     df_test = df.iloc[test_inds]
 
-    # refit 
-    print(f"[INFO:] Refitting the model with the best parameters... on {len(df_full_train)} samples.")
-    model = refit_model(df_full_train, params, freq="1h")
+    # set new columns for best models
+    best_models["MAE_test"] = None
+    best_models["RMSE_test"] = None
 
-    print(df_full_train.tail())
+    # iterate and refit the models
+    for i, params in enumerate(top_n_params):
+        print(f"[INFO:] Refitting model {i+1} of {top_n_models}...")
 
-    # evaluate
-    print(df_test)
-    print(f"[INFO:] Evaluating the model on the test set... on {len(df_test)} samples.")
-    results = evaluate_model(df_test, model)
-    print(results)
+        # fit model
+        model = refit_model(df_full_train, params, freq="1h")
+       
+        # evaluate
+        print(f"[INFO:] Evaluating the model on the test set... on {len(df_test)} samples.")
+        metrics = evaluate_model(df_test, model)
 
+        # get the MAE and RMSE from the results (they are called val but they are from the test set)
+        mae_test = metrics['MAE_val'].values[0]
+        rmse_test = metrics['RMSE_val'].values[0]
+
+        # add to best models df 
+        best_models.loc[i, "mae_test"] = mae_test
+        best_models.loc[i, "rmse_test"] = rmse_test        
+
+    # save the results
+    best_models.to_csv(results_path / 'np_results.csv', index=False)
 
 if __name__ == "__main__":
     main()
