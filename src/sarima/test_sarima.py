ac63f4d4c53c261926c9fd1c956f2e5c72eada97
MinaAlmasi <mina.almasi00@gmail.com>
2024-05-22 13:34:15 +0200

    fix: adapt sarima pipeline to other stops

--- src/sarima/test_sarima.py
+++ src/sarima/test_sarima.py
@@ -25,15 +25,7 @@ def evaluate_model(model_fit, df_test, save_path=None):
     
     return mae_test, rmse_test, y_pred
 
-def main(): 
-    # set paths
-    path = pathlib.Path(__file__)
-    data_path = path.parents[2] / "data"
-    results_path = path.parents[2] / "results"
-
-    # load data
-    df = pd.read_csv(data_path / 'processed_1A_norreport.csv')
-
+def stop_pipeline(df, forecast_path, results_path, save_test_only=False):
     # impute missing values
     df = impute_missing(df, method='rolling', window=24)
 
@@ -62,18 +54,65 @@ def main():
     model_fit = model.fit()
 
     # evaluate model
-    mae_test, rmse_test, y_pred = evaluate_model(model_fit, df_test, save_path=results_path / "forecasts" / 'sarima_forecast.csv')
+    mae_test, rmse_test, y_pred = evaluate_model(model_fit, df_test, save_path=forecast_path / 'sarima_forecast.csv')
     print(f"MAE test: {mae_test}")
     print(f"RMSE test: {rmse_test}")
 
-    # save results with the train-val results 
-    train_val_df = pd.read_csv(results_path / 'sarima_train_val.csv')
+    if save_test_only:
+        # save results with the test results only
+        results = pd.DataFrame({'mae_test': [mae_test], 'rmse_test': [rmse_test]})
+        results.to_csv(results_path / 'sarima_results.csv', index=False)
+    
+    else:
+        # save results with the train-val results 
+        train_val_df = pd.read_csv(results_path / 'sarima_train_val.csv')
 
-    # add the test results
-    train_val_df['mae_test'] = mae_test
-    train_val_df['rmse_test'] = rmse_test
+        # add the test results
+        train_val_df['mae_test'] = mae_test
+        train_val_df['rmse_test'] = rmse_test
 
-    train_val_df.to_csv(results_path / 'sarima_results.csv', index=False)
+        train_val_df.to_csv(results_path / 'sarima_results.csv', index=False)
+        
+
+def main(): 
+    # set paths
+    path = pathlib.Path(__file__)
+    data_path = path.parents[2] / "data" / "clean_stops"
+    results_path = path.parents[2] / "results"
+
+    # load the nørreport data
+    df = pd.read_csv(data_path / 'clean_1A_norreport.csv')
+
+        
+    # run on norreport
+    norreport_results_path = results_path / "norreport"
+    norreport_forecast_path = norreport_results_path / "forecasts"
+    norreport_results_path.mkdir(parents=True, exist_ok=True)
+    norreport_forecast_path.mkdir(parents=True, exist_ok=True)
+
+    stop_pipeline(df, norreport_forecast_path, norreport_results_path, save_test_only=False)
+
+    # run on all stops
+    for stop in data_path.iterdir():
+        if stop.name == 'clean_1A_norreport.csv': # skip norreport as it has its seperate pipeline
+            continue
+        
+        # load the data
+        df = pd.read_csv(stop)
+
+        # stop name (remove clean_1A_ and .csv) from name
+        stop_name = stop.stem[9:]
+
+        # results path 
+        other_results_path = results_path / "other_stops" / stop_name
+        other_results_path.mkdir(parents=True, exist_ok=True)
+
+        # run the pipeline
+        stop_pipeline(df, other_results_path, other_results_path, save_test_only=True)
+    
+
+
+    
 
 
 if __name__ == "__main__":
